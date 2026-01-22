b58c2e7f7636f8a602f1e93bc7ad85620bee827e
tejaskalal <tejaskalal1125@gmail.com>
2026-01-22 23:58:24 +0530

    feat: implement landing page and frontend structure

---
+++ frontend/src/pages/LandingPage.jsx
@@ -0,0 +1,37 @@
+import "../App.css";
+import { Link } from "react-router-dom";
+
+const LandingPage = () => {
+  return (
+    <>
+      <div className="landingPageContainer">
+        <nav>
+          <div className="navHeader">
+            <h2>Confero</h2>
+          </div>
+          <div className="navList">
+            <p>Join as guest</p>
+            <p>Register</p>
+            <div role="button">
+              <p>Login</p>
+            </div>
+          </div>
+        </nav>
+        <div className="landingMainContainer">
+          <div>
+            <h1 style={{ color: "#FF9839" }}>Be there, from anywhere.</h1>
+            <p>Cover all your meetings in one place</p>
+            <div role="button">
+              <Link to="/home">Get Started</Link>
+            </div>
+          </div>
+          <div>
+            <img src="public/mobile.png" alt="mobile_img" />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default LandingPage;
