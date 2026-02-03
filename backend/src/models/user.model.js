5ca33d7907e93ab2af204860172b04e8d574f5ff
tejaskalal <tejaskalal1125@gmail.com>
2026-02-03 22:36:19 +0530

    fix:schema for meetings

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -2,14 +2,12 @@ import mongoose from "mongoose";
 
 const { Schema } = mongoose;
 
-const userSchema = new Schema(
-  {
-    name: { type: String, required: true },
-    username: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
-  },
-  { timestamps: true },
-);
+const userSchema = new Schema({
+  name: { type: String, required: true },
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  token: { type: String },
+});
 
 const User = mongoose.model("User", userSchema);
 
