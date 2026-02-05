297ce80f8c35f2ebfe64fe29fd72ec2f51c91575
tejaskalal <tejaskalal1125@gmail.com>
2026-02-05 08:38:04 +0530

    fix: fix import typo in authcontext file

--- frontend/src/contexts/AuthContext.jsx
+++ frontend/src/contexts/AuthContext.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import httpStatus from "http-status";
 import { createContext, useContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import server from "../environment";
+import server from "../enviornment";
 
 export const AuthContext = createContext({});
 
