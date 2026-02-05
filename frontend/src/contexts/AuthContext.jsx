01df6a8b22596431fc202fa195e6934e9893b7f4
tejaskalal <tejaskalal1125@gmail.com>
2026-02-05 09:13:21 +0530

    fixed typos

--- frontend/src/contexts/AuthContext.jsx
+++ frontend/src/contexts/AuthContext.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import httpStatus from "http-status";
 import { createContext, useContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import server from "../enviornment";
+import server from "../environment";
 
 export const AuthContext = createContext({});
 
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React, { useContext, useState } from "react";
-import withAuth from "../utils/WithAuth";
+import WithAuth from "../utils/WithAuth";
 import { useNavigate } from "react-router-dom";
 import "../App.css";
 import { Button, IconButton, TextField } from "@mui/material";
@@ -70,4 +70,4 @@ function Home() {
   );
 }
 
-export default withAuth(Home);
+export default WithAuth(Home);
